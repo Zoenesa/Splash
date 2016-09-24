066c472cb7b1f47ac2eac2e9d9bd559ef3ee68f5
Zoenesa <zoenesa@gmail.com>
2016-09-24 18:56:56 +0700

    fix startup

---
+++ StartUp/FormStartUp.designer.vb
@@ -0,0 +1,194 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormStartUp
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FormStartUp))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.lblStatus = New System.Windows.Forms.Label()
+        Me.UpdateTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.pnlStatus = New System.Windows.Forms.Panel()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.TableLayoutPanel2 = New System.Windows.Forms.TableLayoutPanel()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.lblTimeRemaining = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.TableLayoutPanel1.SuspendLayout()
+        Me.TableLayoutPanel2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.PictureBox1.Location = New System.Drawing.Point(3, 3)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(136, 137)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'lblStatus
+        '
+        Me.lblStatus.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblStatus.ForeColor = System.Drawing.Color.White
+        Me.lblStatus.Location = New System.Drawing.Point(145, 176)
+        Me.lblStatus.Margin = New System.Windows.Forms.Padding(3, 3, 3, 4)
+        Me.lblStatus.Name = "lblStatus"
+        Me.lblStatus.Size = New System.Drawing.Size(353, 23)
+        Me.lblStatus.TabIndex = 1
+        Me.lblStatus.Text = "Status"
+        Me.lblStatus.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'UpdateTimer
+        '
+        '
+        'pnlStatus
+        '
+        Me.pnlStatus.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.pnlStatus.Location = New System.Drawing.Point(3, 176)
+        Me.pnlStatus.Name = "pnlStatus"
+        Me.pnlStatus.Size = New System.Drawing.Size(136, 24)
+        Me.pnlStatus.TabIndex = 3
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.ColumnCount = 2
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 28.34331!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 71.65668!))
+        Me.TableLayoutPanel1.Controls.Add(Me.PictureBox1, 0, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.pnlStatus, 0, 2)
+        Me.TableLayoutPanel1.Controls.Add(Me.lblStatus, 1, 2)
+        Me.TableLayoutPanel1.Controls.Add(Me.TableLayoutPanel2, 1, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.lblTimeRemaining, 1, 1)
+        Me.TableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(0, 0)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 3
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20.0!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(501, 203)
+        Me.TableLayoutPanel1.TabIndex = 4
+        '
+        'TableLayoutPanel2
+        '
+        Me.TableLayoutPanel2.ColumnCount = 2
+        Me.TableLayoutPanel2.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 55.08475!))
+        Me.TableLayoutPanel2.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 44.91525!))
+        Me.TableLayoutPanel2.Controls.Add(Me.Label2, 0, 0)
+        Me.TableLayoutPanel2.Controls.Add(Me.Label3, 1, 0)
+        Me.TableLayoutPanel2.Controls.Add(Me.Label4, 0, 1)
+        Me.TableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TableLayoutPanel2.Location = New System.Drawing.Point(145, 3)
+        Me.TableLayoutPanel2.Name = "TableLayoutPanel2"
+        Me.TableLayoutPanel2.RowCount = 2
+        Me.TableLayoutPanel2.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 40.87591!))
+        Me.TableLayoutPanel2.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 59.12409!))
+        Me.TableLayoutPanel2.Size = New System.Drawing.Size(353, 137)
+        Me.TableLayoutPanel2.TabIndex = 4
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(4, 4)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 4, 3, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(187, 51)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "SPLASH" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "APLIKASI DATABASE"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Label3.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(198, 0)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 3, 4)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(152, 51)
+        Me.Label3.TabIndex = 1
+        Me.Label3.Text = "Versi"
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.BottomLeft
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Label4.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.White
+        Me.Label4.Location = New System.Drawing.Point(4, 100)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(4, 45, 3, 2)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(187, 35)
+        Me.Label4.TabIndex = 1
+        Me.Label4.Text = "Aplikasi database dengan menggunakan MYSQL database" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
+        'lblTimeRemaining
+        '
+        Me.lblTimeRemaining.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTimeRemaining.ForeColor = System.Drawing.Color.White
+        Me.lblTimeRemaining.Location = New System.Drawing.Point(145, 146)
+        Me.lblTimeRemaining.Margin = New System.Windows.Forms.Padding(3, 3, 3, 4)
+        Me.lblTimeRemaining.Name = "lblTimeRemaining"
+        Me.lblTimeRemaining.Size = New System.Drawing.Size(353, 23)
+        Me.lblTimeRemaining.TabIndex = 1
+        Me.lblTimeRemaining.Text = "Status"
+        Me.lblTimeRemaining.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'FormStartUp
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(149, Byte), Integer), CType(CType(149, Byte), Integer), CType(CType(149, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(501, 203)
+        Me.Controls.Add(Me.TableLayoutPanel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "FormStartUp"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "FormStartUp"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.TableLayoutPanel2.ResumeLayout(False)
+        Me.TableLayoutPanel2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents lblStatus As Label
+    Private WithEvents UpdateTimer As Timer
+    Friend WithEvents pnlStatus As Panel
+    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
+    Friend WithEvents TableLayoutPanel2 As TableLayoutPanel
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents lblTimeRemaining As Label
+End Class
---
+++ StartUp/FormStartUp.vb
@@ -0,0 +1,299 @@
+﻿Imports System.Drawing.Drawing2D
+Imports System.IO
+Imports System.Threading
+Imports System.Xml
+
+Public Class FormStartUp
+
+    ' Threading
+    Private Shared ms_frmSplash As FormStartUp = Nothing
+    Private Shared ms_oThread As Thread = Nothing
+
+    ' Fade in and out.
+    Private m_dblOpacityIncrement As Double = 0.05
+    Private m_dblOpacityDecrement As Double = 0.08
+    Private Const TIMER_INTERVAL As Integer = 50
+
+    ' Status and progress bar
+    Private m_sStatus As String
+    Private m_sTimeRemaining As String
+    Private m_dblCompletionFraction As Double = 0.0
+    Private m_rProgress As Rectangle
+
+    ' Progress smoothing
+    Private m_dblLastCompletionFraction As Double = 0.0
+    Private m_dblPBIncrementPerTimerInterval As Double = 0.015
+
+    ' Self-calibration support
+    Private m_iIndex As Integer = 1
+    Private m_iActualTicks As Integer = 0
+    Private m_alPreviousCompletionFraction As ArrayList
+    Private m_alActualTimes As New ArrayList()
+    Private m_dtStart As DateTime
+    Private m_bFirstLaunch As Boolean = False
+    Private m_bDTSet As Boolean = False
+
+    Shared Sub New()
+        ms_frmSplash = Nothing
+        ms_oThread = Nothing
+    End Sub
+
+    Public Sub New()
+        InitializeComponent()
+        Me.Opacity = 0.0
+        UpdateTimer.Interval = TIMER_INTERVAL
+        UpdateTimer.Start()
+    End Sub
+
+    Public Shared Sub ShowSplashScreen()
+        ' Make sure it's only launched once.
+        If Not (ms_frmSplash Is Nothing) Then
+            Return
+        End If
+        ms_oThread = New Thread(New ThreadStart(AddressOf FormStartUp.ShowForm))
+        ms_oThread.IsBackground = True
+        ms_oThread.SetApartmentState(ApartmentState.STA)
+        ms_oThread.Start()
+        While ms_frmSplash Is Nothing OrElse ms_frmSplash.IsHandleCreated = False
+            System.Threading.Thread.Sleep(TIMER_INTERVAL)
+        End While
+    End Sub
+
+    Public Shared Sub CloseForm()
+        If Not (ms_frmSplash Is Nothing) AndAlso ms_frmSplash.IsDisposed = False Then
+            ' Make it start going away.
+            ms_frmSplash.m_dblOpacityIncrement = -ms_frmSplash.m_dblOpacityDecrement
+        End If
+        ms_oThread = Nothing
+        ' we don't need these any more.
+        ms_frmSplash = Nothing
+    End Sub
+
+    Public Shared Sub SetStatus(newStatus As String)
+        SetStatus(newStatus, True)
+    End Sub
+
+    Public Shared Sub SetStatus(newStatus As String, setReference As Boolean)
+        If ms_frmSplash Is Nothing Then
+            Return
+        End If
+        ms_frmSplash.m_sStatus = newStatus
+        If setReference Then
+            ms_frmSplash.SetReferenceInternal()
+        End If
+    End Sub
+
+    Public Shared Sub SetReferencePoint()
+        If ms_frmSplash Is Nothing Then
+            Return
+        End If
+        ms_frmSplash.SetReferenceInternal()
+
+    End Sub
+
+    Private Shared Sub ShowForm()
+        ms_frmSplash = New FormStartUp()
+        Application.Run(ms_frmSplash)
+    End Sub
+
+    Private Sub SetReferenceInternal()
+        If m_bDTSet = False Then
+            m_bDTSet = True
+            m_dtStart = DateTime.Now
+            ReadIncrements()
+        End If
+        Dim dblMilliseconds As Double = ElapsedMilliSeconds()
+        m_alActualTimes.Add(dblMilliseconds)
+        m_dblLastCompletionFraction = m_dblCompletionFraction
+        If Not (m_alPreviousCompletionFraction Is Nothing) AndAlso m_iIndex < m_alPreviousCompletionFraction.Count Then
+            m_dblCompletionFraction = DirectCast(m_alPreviousCompletionFraction(System.Math.Max(System.Threading.Interlocked.Increment(m_iIndex), m_iIndex - 1)), Double)
+        Else
+            m_dblCompletionFraction = If((m_iIndex > 0), 1, 0)
+        End If
+    End Sub
+
+    Private Function ElapsedMilliSeconds() As Double
+        Dim ts As TimeSpan = DateTime.Now - m_dtStart
+        Return ts.TotalMilliseconds
+    End Function
+
+    Private Sub ReadIncrements()
+        Dim sPBIncrementPerTimerInterval As String = SplashScreenXMLStorage.Interval
+        Dim dblResult As Double
+
+        If [Double].TryParse(sPBIncrementPerTimerInterval, System.Globalization.NumberStyles.Float, System.Globalization.NumberFormatInfo.InvariantInfo, dblResult) = True Then
+            m_dblPBIncrementPerTimerInterval = dblResult
+        Else
+            m_dblPBIncrementPerTimerInterval = 0.0015
+        End If
+
+        Dim sPBPreviousPctComplete As String = SplashScreenXMLStorage.Percents
+
+        If sPBPreviousPctComplete <> "" Then
+            Dim aTimes As String() = sPBPreviousPctComplete.Split(Nothing)
+            m_alPreviousCompletionFraction = New ArrayList()
+
+            Dim i As Integer = 0
+            While i < aTimes.Length
+                Dim dblVal As Double
+                If [Double].TryParse(aTimes(i), System.Globalization.NumberStyles.Float, System.Globalization.NumberFormatInfo.InvariantInfo, dblVal) = True Then
+                    m_alPreviousCompletionFraction.Add(dblVal)
+                Else
+                    m_alPreviousCompletionFraction.Add(1.0)
+                End If
+                System.Math.Max(System.Threading.Interlocked.Increment(i), i - 1)
+            End While
+        Else
+            m_bFirstLaunch = True
+            m_sTimeRemaining = ""
+        End If
+    End Sub
+
+    Private Sub StoreIncrements()
+        Dim sPercent As String = ""
+        Dim dblElapsedMilliseconds As Double = ElapsedMilliSeconds()
+        Dim i As Integer = 0
+        While i < m_alActualTimes.Count
+            sPercent += (DirectCast(m_alActualTimes(i), Double) / dblElapsedMilliseconds).ToString("0.####", System.Globalization.NumberFormatInfo.InvariantInfo) + " "
+            System.Math.Max(System.Threading.Interlocked.Increment(i), i - 1)
+        End While
+
+        SplashScreenXMLStorage.Percents = sPercent
+
+        m_dblPBIncrementPerTimerInterval = 1.0 / CDbl(m_iActualTicks)
+
+        SplashScreenXMLStorage.Interval = m_dblPBIncrementPerTimerInterval.ToString("#.000000", System.Globalization.NumberFormatInfo.InvariantInfo)
+    End Sub
+
+    Public Shared Function GetSplashScreen() As FormStartUp
+        Return ms_frmSplash
+    End Function
+
+    Private Sub CekInisialKonpig()
+
+    End Sub
+
+    Private Sub CekInisialKonektor()
+
+    End Sub
+
+    Private Function CekInisialFolder() As Boolean
+        Dim flag As Boolean
+
+
+        Return flag
+    End Function
+
+    Private Sub UpdateTimer_Tick(sender As Object, e As EventArgs) Handles UpdateTimer.Tick
+        Me.lblStatus.Text = Me.m_sStatus
+        If (Me.m_dblOpacityIncrement > 0) Then
+            Me.m_iActualTicks += 1
+            If (MyBase.Opacity < 1) Then
+                MyBase.Opacity = (MyBase.Opacity + Me.m_dblOpacityIncrement)
+            End If
+        ElseIf (MyBase.Opacity > 0) Then
+            MyBase.Opacity = (MyBase.Opacity + Me.m_dblOpacityIncrement)
+        Else
+            Me.StoreIncrements()
+            Me.UpdateTimer.Stop()
+            MyBase.Close()
+        End If
+        If (Not Me.m_bFirstLaunch AndAlso (Me.m_dblLastCompletionFraction < Me.m_dblCompletionFraction)) Then
+            Me.m_dblLastCompletionFraction = (Me.m_dblLastCompletionFraction + Me.m_dblPBIncrementPerTimerInterval)
+            Dim width As Integer = CInt(Math.Floor(CDbl((Me.pnlStatus.ClientRectangle.Width * Me.m_dblLastCompletionFraction))))
+            Dim height As Integer = Me.pnlStatus.ClientRectangle.Height
+            Dim x As Integer = Me.pnlStatus.ClientRectangle.X
+            Dim y As Integer = Me.pnlStatus.ClientRectangle.Y
+            If ((width > 0) AndAlso (height > 0)) Then
+                Me.m_rProgress = New Rectangle(x, y, width, height)
+                If Not Me.pnlStatus.IsDisposed Then
+                    Dim graphics As Graphics = Me.pnlStatus.CreateGraphics
+                    Dim brush As New LinearGradientBrush(Me.m_rProgress, Color.FromArgb(58, 96, 151), Color.FromArgb(181, 237, 254), LinearGradientMode.Horizontal)
+                    graphics.FillRectangle(brush, Me.m_rProgress)
+                    graphics.Dispose()
+                End If
+                Dim num6 As Integer = (1 + (CInt((50 * ((1 - Me.m_dblLastCompletionFraction) / Me.m_dblPBIncrementPerTimerInterval))) / 1000))
+                Me.m_sTimeRemaining = If((num6 = 1), String.Format("1 second remaining", New Object(0 - 1) {}), String.Format("{0} seconds remaining", num6))
+            End If
+        End If
+        Me.lblTimeRemaining.Text = Me.m_sTimeRemaining
+
+    End Sub
+
+    Friend Class SplashScreenXMLStorage
+        Private Shared ms_StoredValues As String = "SplashScreen.xml"
+        Private Shared ms_DefaultPercents As String = ""
+        Private Shared ms_DefaultIncrement As String = ".015"
+
+
+        ' Get or set the string storing the percentage complete at each checkpoint.
+        Public Shared Property Percents() As String
+            Get
+                Return GetValue("Percents", ms_DefaultPercents)
+            End Get
+            Set
+                SetValue("Percents", Value)
+            End Set
+        End Property
+        ' Get or set how much time passes between updates.
+        Public Shared Property Interval() As String
+            Get
+                Return GetValue("Interval", ms_DefaultIncrement)
+            End Get
+            Set
+                SetValue("Interval", Value)
+            End Set
+        End Property
+
+        ' Store the file in a location where it can be written with only User rights. (Don't use install directory).
+        Private Shared ReadOnly Property StoragePath() As String
+            Get
+                Return Path.Combine(Application.UserAppDataPath, ms_StoredValues)
+            End Get
+        End Property
+
+        ' Helper method for getting inner text of named element.
+        Private Shared Function GetValue(name As String, defaultValue As String) As String
+            If Not File.Exists(StoragePath) Then
+                Return defaultValue
+            End If
+
+            Try
+                Dim docXML As New XmlDocument()
+                docXML.Load(StoragePath)
+                Dim elValue As XmlElement = TryCast(docXML.DocumentElement.SelectSingleNode(name), XmlElement)
+                Return If((elValue Is Nothing), defaultValue, elValue.InnerText)
+            Catch
+                Return defaultValue
+            End Try
+        End Function
+
+        ' Helper method for setting inner text of named element.  Creates document if it doesn't exist.
+        Public Shared Sub SetValue(name As String, stringValue As String)
+            Dim docXML As New XmlDocument()
+            Dim elRoot As XmlElement = Nothing
+            If Not File.Exists(StoragePath) Then
+                elRoot = docXML.CreateElement("root")
+                docXML.AppendChild(elRoot)
+            Else
+                docXML.Load(StoragePath)
+                elRoot = docXML.DocumentElement
+            End If
+            Dim value As XmlElement = TryCast(docXML.DocumentElement.SelectSingleNode(name), XmlElement)
+            If value Is Nothing Then
+                value = docXML.CreateElement(name)
+                elRoot.AppendChild(value)
+            End If
+            value.InnerText = stringValue
+            docXML.Save(StoragePath)
+        End Sub
+    End Class
+
+    Private Sub TableLayoutPanel1_Paint(sender As Object, e As PaintEventArgs) Handles TableLayoutPanel1.Paint
+
+    End Sub
+
+    Private Sub TableLayoutPanel1_DoubleClick(sender As Object, e As EventArgs) Handles TableLayoutPanel1.DoubleClick
+        FormStartUp.CloseForm()
+    End Sub
+End Class
