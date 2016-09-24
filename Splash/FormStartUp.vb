e7c938bdc545ee9a567cf4f7dea83e33f1312b5b
agung <agungjawata@hotmail.com>
2016-09-24 15:05:21 +0700

    code

--- Splash/FormStartUp.vb
+++ Splash/FormStartUp.vb
@@ -1,3 +1,24 @@
 ﻿Public Class FormStartUp
+    Private Sub FormStartUp_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub CekInisialKonpig()
+
+    End Sub
+
+    Private Sub CekInisialKonektor()
+
+    End Sub
+
+    Private Function CekInisialFolder() As Boolean
+        Dim flag As Boolean
+
+
+
+
+        Return flag
+    End Function
+
 
 End Class
